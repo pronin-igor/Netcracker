72a0b2aa96c647e166c5969174de08405c27916b
pronin-igor <gosha.pronin.2000@icloud.com>
2021-12-09 22:03:15 +0300

    fix

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -6,17 +6,23 @@ const inputsDiv = document.createElement("div");
 const treeDiv = document.createElement("div");
 
 const button = document.createElement("button");
+button.classList.add("inputAndButton");
 button.innerText = "Добавить элемент";
 const button1 = document.createElement("button");
+button1.classList.add("inputAndButton");
 button1.innerText = "Посмотреть элемент";
 const button2 = document.createElement("button");
+button2.classList.add("inputAndButton");
 button2.innerText = "Удалить элемент";
 
 const input = document.createElement("input");
+input.classList.add("inputAndButton");
 input.type = "text";
 const input1 = document.createElement("input");
+input1.classList.add("inputAndButton");
 input1.type = "text";
 const input2 = document.createElement("input");
+input2.classList.add("inputAndButton");
 input2.type = "text";
 
 buttonsDiv.appendChild(button);
@@ -33,36 +39,42 @@ document.body.appendChild(treeDiv);
 
 const tree = new Tree<number>();
 
-function showTree(list: List<number> | null, element: HTMLElement): void {
+function showTree(list: List<number> | null, element: HTMLElement, transform: string): void {
     if (list != null) {
-        const ul = document.createElement("ul");
-        element.appendChild(ul);
-        const li = document.createElement("li");
-        ul.appendChild(li);
-        const a = document.createElement("a");
-        a.href = "#";
-        a.innerText = "" + list.data;
-        li.appendChild(a);
-        showTree(list.left, ul);
-        showTree(list.right, ul);
+        const parentDiv = document.createElement("div");
+        parentDiv.classList.add("parentDivStyle");
+        parentDiv.style.transform = transform;
+        element.appendChild(parentDiv);
+        const p = document.createElement("p");
+        p.innerText = "" + list.data;
+        p.classList.add("pStyle");
+        parentDiv.appendChild(p);
+        const childDiv = document.createElement("div");
+        childDiv.classList.add("childDivStyle");
+        parentDiv.appendChild(childDiv);
+        showTree(list.left, childDiv, "translateX(-50px)");
+        showTree(list.right, childDiv, "translateX(+50px)");
     }
 }
 
-function highlightElement(list: List<number> | null, element: HTMLElement, data: number): void {
+function highlightElement(list: List<number> | null, element: HTMLElement, data: number, transform: string): void {
     if (list != null) {
-        const ul = document.createElement("ul");
-        element.appendChild(ul);
-        const li = document.createElement("li");
-        ul.appendChild(li);
-        const a = document.createElement("a");
-        a.href = "#";
-        a.innerText = "" + list.data;
+        const parentDiv = document.createElement("div");
+        parentDiv.classList.add("parentDivStyle");
+        parentDiv.style.transform = transform;
+        element.appendChild(parentDiv);
+        const p = document.createElement("p");
+        p.innerText = "" + list.data;
+        p.classList.add("pStyle");
         if (list.data === data) {
-            a.style.color = "red";
+            p.style.color = "red";
         }
-        li.appendChild(a);
-        highlightElement(list.left, ul, data);
-        highlightElement(list.right, ul, data);
+        parentDiv.appendChild(p);
+        const childDiv = document.createElement("div");
+        childDiv.classList.add("childDivStyle");
+        parentDiv.appendChild(childDiv);
+        highlightElement(list.left, childDiv, data, "translateX(-50px)");
+        highlightElement(list.right, childDiv, data, "translateX(+50px)");
     }
 }
 
@@ -76,14 +88,14 @@ button.onclick = (): void => {
     clearDiv(treeDiv);
     const field = input.value;
     tree.insert(+field);
-    showTree(tree.root, treeDiv);
+    showTree(tree.root, treeDiv, "");
 };
 
 button1.onclick = (): void => {
     clearDiv(treeDiv);
     const field1 = input1.value;
     if (tree.search(tree.root, +field1) != null) {
-        highlightElement(tree.root, treeDiv, +field1);
+        highlightElement(tree.root, treeDiv, +field1, "");
     } else {
         console.log("Элемент с заданным ключом не найден");
     }
@@ -93,6 +105,6 @@ button2.onclick = (): void => {
     clearDiv(treeDiv);
     const field2 = input2.value;
     tree.remove(tree.root, +field2);
-    showTree(tree.root, treeDiv);
+    showTree(tree.root, treeDiv, "");
 };
 
